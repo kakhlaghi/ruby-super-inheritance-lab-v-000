9f665a4bde8a6c299176692b057dae26825ea99b
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-05 18:06:40 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,24 +6,16 @@ class ChattyStudent < Student
   end
   
   def raise_hand
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
-      super
-      puts "Pick me!"
+     super
+     super
+     super
+     super
+     super
+     super
+     super
+     super
+     super
+     
     
   end    
 end
